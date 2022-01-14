936e52ec249addfe6fcbb2eba6991c5d3aa0eda0
NastiaIvashova <90038890+NastiaIvashova@users.noreply.github.com>
2022-01-14 20:45:42 +0200

    Delete pizza-delivery directory

--- pizza-delivery/src/main/java/com/pizzarestaurant/pizzadelivery/repository/PizzaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pizzarestaurant.pizzadelivery.repository;
-
-import com.pizzarestaurant.pizzadelivery.model.Pizza;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PizzaRepository extends JpaRepository<Pizza, Long> {
-}
